81306dc8e3b36aec9a760263856e65f3fbdbf715
Rada Berar <ujagaga@gmail.com>
2022-05-23 00:12:05 +0200

    Separated daemon from onetime

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 A Python script for simpler email sending from embedded devices and scripts.
 I have my own home IOT ecosystem comprised of various WiFi devices on the same network. Most of them are low resource devices capable of performing an http get request or even an MQTT post.
 
-This script can be run in a one time mode with a text message as a parameter to send email to a predefined address using a predefined account. 
-You can override the email subject and recipient, but the email sending setup is hard coded into the script, so before use, alter the parameters. 
+The "email_send.py" is to be run in a one time mode with a text message as a parameter to send email to a predefined address using a predefined account. 
+Run the script once to create a "config.json" file which you can then adjust to your needs.
 
-Another use case is running in "daemon" mode. You can select HTTP and/or MQTT for access method. If you choose HTTP, the script will keep running an http server listening on port 5555 by default, 
-but you can change it using "-p" parameter. Then just make a simple GET request with parameters to use for email like:
+The "email_send_daemon.py" can run in "daemon" mode. You can select HTTP and/or MQTT for access method. If you choose HTTP, the script will keep running an http server listening on port 5555 by default, 
+but you can change it in the script. Then just make a simple GET request with parameters to use for email like:
 
 
-	<script_ip>:5555/?m="Some message text"&r="optional@recipient.email"&s="email subject"
+	<script_ip>:5555/?m="Some message text"&r="optional@recipient.email"&s="email subject"&a="list of attachments"
 	
 	
 If you use MQTT, it will subscribe to a local MQTT server on default port 1883, on topic "email" and respond via post to topic "email_response".
@@ -18,7 +18,7 @@ If you use MQTT, it will subscribe to a local MQTT server on default port 1883, 
 The message text (on both http and MQTT) can be a simple string or a Json formated string to override the subject and/or recipient like:
 
 
-	mosquitto_pub -h <script_host_ip> -m '{"m":"Some message to send", "r":"new@recipient.email", "s":"email subject"}'
+	mosquitto_pub -h <script_host_ip> -m '{"m":"Some message to send", "r":"new@recipient.email", "s":"email subject", "a":"list of files to attach"}'
 
 
 ## How to start ##
@@ -30,13 +30,6 @@ If you intend to use the MQTT feature, make sure you have "paho-mqtt" Python lib
 	
 	
 Then run the script with "-h" parameter for help. This will give you an overview of parameters.
-
-
-
-## Note to myself ##
-
-In the public version of this repo, the email parameters are removed for privacy. In the private version they are set: 
-git@github.com:ujagaga/email_send.git
 
 
 ## Contact ##
--- email_send_daemon.py
+++ email_send_daemon.py
@@ -6,14 +6,23 @@ To send email using Gmail, go to your gmail account setup, choose "Security" and
 Then click on "App passwords" and add any app name (e.g. PythonApp) to get an auto generated SENDER_PASSWORD. Use this SENDER_PASSWORD
 (without spaces) as the email SENDER_PASSWORD.
 
+This script can send an email from the terminal and if run as a daemon, it will listen to "email" topic on a local
+MQTT server and send what ever it receives (e.g mosquitto_pub -h localhost -t email -m "Some message to send").
+It will also listen to an HTTP connection get request and send whatever arrives as a "m" parameter.
+( e.g. http://localhost:5555/?m="Some%20message%20to%20send")
+
 The message can also be a json formatted string like:
-"{'m':'<msg to send>', 'r':'<optional recipient email>', 's':'<optional email subject>', 'a':'<optional file list>'}"
+"{m:<msg to send>, r:<optional recipient email>, s:<optional email subject>}"
+
+Before yopu start, make sure you have paho-mqtt library installed.
 """
 
 import json
 import argparse
 import smtplib
 import ssl
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
 import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -21,6 +30,11 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 
+MQTT_RX_TOPIC = "email"
+MQTT_TX_TOPIC = "email_response"
+MQTT_SERVER_ADDR = "localhost"
+MQTT_SERVER_PORT = 1883
+HTTP_PORT = 5555
 SMTP_PORT = 587
 SMTP_USE_STARTTLS = True
 SENDER_EMAIL = "youremail@gmail.com"
@@ -29,6 +43,7 @@ SMTP_SERVER = "smtp.gmail.com"
 recipient = "some@default.recipient"
 subject = "Notification from IOT Portal"
 sender_name = "OC IOT portal"
+receiver = None
 CFG_FILE = "config.json"
 
 
@@ -93,6 +108,67 @@ def sendmail(message, msg_recipient=recipient, msg_subject=subject, attach_file_
         return "ERROR: {}".format(e)
 
 
+class MqttStatusReceiver:
+    def __init__(self, host=MQTT_SERVER_ADDR, port=MQTT_SERVER_PORT, topic=MQTT_RX_TOPIC):
+        self.host = host
+        self.port = port
+        self.topic = topic
+        self.rx_msg = None
+
+        self.client = mqtt.Client('email_sender')
+        try:
+            self.client.connect(host, port=port)
+            self.client.on_connect = self.on_connect
+            self.client.on_message = self.on_message
+            self.client.loop_start()
+        except:
+            pass
+
+    def on_connect(self, client, userdata, flags, rc):
+        client.subscribe(self.topic)
+
+    def on_message(self, client, userdata, msg):
+        self.rx_msg = msg.payload.decode()
+        if self.rx_msg is not None:
+            response = sendmail(self.rx_msg)
+            print(response)
+            self.respond(response)
+
+    def stop(self):
+        self.client.disconnect()
+        self.client.loop_stop()
+
+    def respond(self, message):
+        client = mqtt.Client('emailTx')
+        client.connect(self.host, port=self.port)
+        client.publish(MQTT_TX_TOPIC, message)
+
+
+class HttpHandler(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_response()
+
+        query = urlparse(self.path).query
+        data_dict = parse_qs(query)
+
+        try:
+
+            msg_recipient = data_dict.get("r", [recipient])[0]
+            msg_subject = data_dict.get("s", [subject])[0]
+            msg_attachments = data_dict.get("a", [])[0]
+            msg_text = data_dict["m"][0]
+
+            result = sendmail(msg_text, msg_recipient=msg_recipient, msg_subject=msg_subject, attach_file_list=msg_attachments)
+            self.wfile.write(result.encode('utf-8'))
+        except:
+            self.wfile.write("ERROR: No message text specified. Will not send email.".encode('utf-8'))
+
+
 def load_config():
     global SMTP_PORT
     global SENDER_EMAIL
@@ -128,18 +204,60 @@ if __name__ == '__main__':
     load_config()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--message", help="Text message to send.", required=False)
     parser.add_argument("-s", "--subject", help='Subject of the email. Defaults to: "{}"'.format(subject),
                         required=False, default=subject)
     parser.add_argument("-r", "--recipient", help='Message recipient. Defaults to: "{}"'.format(recipient),
                         required=False, default=recipient)
-    parser.add_argument("-a", "--attachment", help='Comma separated list of files to attach', required=False)
-
+    parser.add_argument("-d", "--daemon", help='Run in background listening to local MQTT topic "email" with response '
+                                               'on MQTT topic "email_response" and/or http get request on port {}. '
+                                               'Valid values: m (use MQTT), h (use HTTP), mh (use both).'
+                                               ''.format(HTTP_PORT), required=False, default="h")
     args = parser.parse_args()
 
     recipient = args.recipient
     subject = args.subject
 
-    if args.message is not None:
-        status = sendmail(args.message, msg_recipient=recipient, msg_subject=subject, attach_file_list=args.attachment)
-        print(status)
+    daemon_http = False
+    daemon_mqtt = False
+    if len(args.daemon) > 0:
+        if args.daemon[0] == 'h':
+            daemon_http = True
+        elif args.daemon[0] == 'm':
+            daemon_mqtt = True
+
+        if len(args.daemon) > 1:
+            if args.daemon[1] == 'h':
+                daemon_http = True
+            elif args.daemon[1] == 'm':
+                daemon_mqtt = True
+
+        if daemon_mqtt:
+            try:
+                import paho.mqtt.client as mqtt
+
+                receiver = MqttStatusReceiver()
+                if not daemon_http:
+                    # Http server not requested, keep an infinite loop for MQTT
+                    try:
+                        while True:
+                            pass
+                    except:
+                        pass
+            except Exception as e:
+                print("ERROR:", e)
+                daemon_mqtt = False
+
+        if daemon_http:
+            # Run the http server
+            server_address = ('0.0.0.0', HTTP_PORT)
+            httpd = HTTPServer(server_address, HttpHandler)
+
+            try:
+                httpd.serve_forever()
+            except:
+                pass
+
+            httpd.server_close()
+
+        if daemon_mqtt:
+            receiver.stop()
